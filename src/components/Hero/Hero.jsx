4ec7a6a4fba3afafbf96427e6b2aaebef0b1eb62
hyzmael88 <31554724+hyzmael88@users.noreply.github.com>
2024-03-05 05:54:21 -0600

    Update Hero component styling

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,12 +6,12 @@ function Hero() {
   return (
     <div className='w-full h-[1190px] flex flex-col px-14 '>
 
-    <div className='w-full h-full flex flex-col justify-center'>
+    <div className='w-full h-full flex flex-col justify-center gap-[23px]'>
       <Image src='/assets/logoHero.png' alt="hero" width={100} height={100} className='w-[497px]' />
-      <h2 className='text-white font-roboto text-8xl font-bold leading-[87.55px] titulo'>
+      <h2 className='w-[656px] text-white font-roboto text-8xl font-bold leading-[87.55px] titulo'>
         Investment & Finances
       </h2>
-      <p className='text-white font-roboto text-xl font-semibold leading-[38.726px]'>
+      <p className='w-[556px] text-white font-roboto text-xl font-semibold leading-[38.726px]'>
         Potencia tus inversiones con nuestra gestión financiera experta. Confía en nuestro equipo para optimizar tu cartera y asegurar un futuro financiero sólido.
       </p>  
     </div>
