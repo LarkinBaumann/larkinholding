cf09ab9f7965300c34a2af74a56f651f470c21c8
hyzmael88 <31554724+hyzmael88@users.noreply.github.com>
2024-03-07 05:56:46 -0600

    Update experience numbers and heading font weight

--- src/components/Acerca/Acerca.jsx
+++ src/components/Acerca/Acerca.jsx
@@ -7,24 +7,24 @@ function Acerca() {
 
     <div className='w-full grid grid-cols-2 gap-x-4 gap-y-8'>
       <div className='w-[302px] h-[228px] border-solid border-[0.723px] border-[#005E93] rounded-[15.901px 15.901px 15.901px 0px] rounded-[16px] flex flex-col justify-center px-[34px] py-[56px]' style={{ background: 'rgba(11, 32, 46, 0.30)' }}>
-        <h4 className='titulo2 text-[76px] font-bold font-header'>25+</h4>
+        <h4 className='titulo2 text-[76px] font-black font-header'>25+</h4>
         <p className='text-white font-header text-[23px] leading-[31px]'>Años de experiencia</p>
       </div>
       <div className='w-[302px] h-[228px] border-solid border-[0.723px] border-[#005E93] rounded-[15.901px 15.901px 15.901px 0px] rounded-[16px] flex flex-col justify-center px-[34px] py-[56px]' style={{ background: 'rgba(11, 32, 46, 0.30)' }}>
-        <h4 className='titulo2 text-[76px] font-bold font-header'>25+</h4>
+        <h4 className='titulo2 text-[76px] font-black font-header'>5K+</h4>
         <p className='text-white font-header text-[23px] leading-[31px]'>Años de experiencia</p>
       </div>
       <div className='w-[302px] h-[228px] border-solid border-[0.723px] border-[#005E93] rounded-[15.901px 15.901px 15.901px 0px] rounded-[16px] flex flex-col justify-center px-[34px] py-[56px]' style={{ background: 'rgba(11, 32, 46, 0.30)' }}>
-        <h4 className='titulo2 text-[76px] font-bold font-header'>25+</h4>
+        <h4 className='titulo2 text-[76px] font-black font-header'>50+</h4>
         <p className='text-white font-header text-[23px] leading-[31px]'>Años de experiencia</p>
       </div>
       <div className='w-[302px] h-[228px] border-solid border-[0.723px] border-[#005E93] rounded-[15.901px 15.901px 15.901px 0px] rounded-[16px] flex flex-col justify-center px-[34px] py-[56px]' style={{ background: 'rgba(11, 32, 46, 0.30)' }}>
-        <h4 className='titulo2 text-[76px] font-bold font-header'>25+</h4>
+        <h4 className='titulo2 text-[76px] font-black font-header'>200+</h4>
         <p className='text-white font-header text-[23px] leading-[31px]'>Años de experiencia</p>
       </div>
     </div>
     <div className='w-full h-full flex flex-col justify-center gap-[16px]'>
-      <h2 className='text-[47px] font-bold text-white leading-[49px]'>
+      <h2 className='text-[47px] font-black text-white leading-[49px]'>
       Potenciando Diversas Empresas, <span className='titulo2'>Uniendo Éxitos</span>.
       </h2>
       <p className='text-white font-paragraph leading-[34px] text-[20px] w-[569px]'>Somos un corporativo diversificado, abarcando industrias clave como bienes raíces, seguridad privada, petróleo y gas, inversiones y gestión financiera. Con enfoque integrador y compromiso con la excelencia, impulsamos el éxito de cada una de nuestras empresas. Nuestro equipo apasionado y profesional se dedica a la innovación y calidad en todo momento.</p>
