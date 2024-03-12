af9ece2b0ba324b96c6060b1d3c7b503a6a3f830
hyzmael88 <31554724+hyzmael88@users.noreply.github.com>
2024-03-12 04:09:54 -0600

    Add react-countup library for number animations

--- src/components/Acerca/Acerca.jsx
+++ src/components/Acerca/Acerca.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react'
 import ShadowAcerca from '../Shadows/ShadowAcerca'
 import { AppContext } from '@/Context/AppContext'
+import CountUp from 'react-countup';
 
 function Acerca() {
 
@@ -11,19 +12,35 @@ function Acerca() {
 
     <div className='w-full grid grid-cols-2 gap-x-4 gap-y-8'>
       <div className='w-[302px] h-[228px] border-solid border-[0.723px] border-[#005E93] rounded-[15.901px 15.901px 15.901px 0px] rounded-[16px] flex flex-col justify-center px-[34px] py-[56px]' style={{ background: 'rgba(11, 32, 46, 0.30)' }}>
-        <h4 className='titulo2 text-[76px] font-black font-header'>{traduccion.acerca.titulo1}</h4>
+        <div className='flex'>
+
+      <CountUp className='titulo2 text-[76px] font-black font-header' end={parseInt(traduccion.acerca.titulo1)} duration={5} />
+      <h4 className='titulo2 text-[76px] font-black font-header'>{traduccion.acerca.complemento1}</h4>
+        </div>
+
         <p className='text-white font-header text-[23px] leading-[31px]'>{traduccion.acerca.descripcion1}</p>
       </div>
       <div className='w-[302px] h-[228px] border-solid border-[0.723px] border-[#005E93] rounded-[15.901px 15.901px 15.901px 0px] rounded-[16px] flex flex-col justify-center px-[34px] py-[56px]' style={{ background: 'rgba(11, 32, 46, 0.30)' }}>
-        <h4 className='titulo2 text-[76px] font-black font-header'>{traduccion.acerca.titulo2}</h4>
-        <p className='text-white font-header text-[23px] leading-[31px]'>{traduccion.acerca.descripcion2}</p>
+      <div className='flex'>
+
+<CountUp className='titulo2 text-[76px] font-black font-header' end={parseInt(traduccion.acerca.titulo2)} duration={5} />
+<h4 className='titulo2 text-[76px] font-black font-header'>{traduccion.acerca.complemento2}</h4>
+  </div>        <p className='text-white font-header text-[23px] leading-[31px]'>{traduccion.acerca.descripcion2}</p>
       </div>
       <div className='w-[302px] h-[228px] border-solid border-[0.723px] border-[#005E93] rounded-[15.901px 15.901px 15.901px 0px] rounded-[16px] flex flex-col justify-center px-[34px] py-[56px]' style={{ background: 'rgba(11, 32, 46, 0.30)' }}>
-        <h4 className='titulo2 text-[76px] font-black font-header'>{traduccion.acerca.titulo3}</h4>
+      <div className='flex'>
+
+<CountUp className='titulo2 text-[76px] font-black font-header' end={parseInt(traduccion.acerca.titulo3)} duration={5} />
+<h4 className='titulo2 text-[76px] font-black font-header'>{traduccion.acerca.complemento3}</h4>
+  </div>
         <p className='text-white font-header text-[23px] leading-[31px]'>{traduccion.acerca.descripcion3}</p>
       </div>
       <div className='w-[302px] h-[228px] border-solid border-[0.723px] border-[#005E93] rounded-[15.901px 15.901px 15.901px 0px] rounded-[16px] flex flex-col justify-center px-[34px] py-[56px]' style={{ background: 'rgba(11, 32, 46, 0.30)' }}>
-        <h4 className='titulo2 text-[76px] font-black font-header'>{traduccion.acerca.titulo4}</h4>
+      <div className='flex'>
+
+<CountUp className='titulo2 text-[76px] font-black font-header' end={parseInt(traduccion.acerca.titulo4)} duration={5} />
+<h4 className='titulo2 text-[76px] font-black font-header'>{traduccion.acerca.complemento4}</h4>
+  </div>
         <p className='text-white font-header text-[23px] leading-[31px]'>{traduccion.acerca.descripcion4}</p>
       </div>
       
