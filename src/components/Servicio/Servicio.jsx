73b1546785872f6bc435056c6ad20113235fe358
hyzmael88 <bartttt15@hotmail.com>
2024-03-15 04:29:05 -0600

    Update Servicio component styles

--- src/components/Servicio/Servicio.jsx
+++ src/components/Servicio/Servicio.jsx
@@ -43,27 +43,27 @@ function Servicio() {
       <ShadowServicio
       inView={inView}
       />
-    <div className="w-full h-[500px] lg:h-[830px] flex flex-col lg:flex-row justify-center items-center px-4 lg:px-14 
+    <div className="w-full h-[500px] lg:h-[830px] flex flex-col lg:flex-row justify-center items-center px-8 lg:px-14 
     max-w-[1444px] mx-auto min-w-sm 
     ">
-    <div className="w-[337px] lg:w-full h-[511px] lg:h-[604px] flex  flex-col lg:flex-row justify-around text-white bg-[#0E192187] items-center rounded-[22px]
+    <div className="w-full h-[511px] lg:h-[604px] flex  flex-col lg:flex-row justify-around text-white bg-[#0E192187] items-center rounded-[22px]
       ">
-      <div className="">
-        <motion.div className="w-[237px] lg:w-[557px] h-[229px] lg:h-[539px]" 
+        <div className="w-full flex flex-col items-center lg:items-start gap-[8px]">
+     
+        <motion.div className="w-[237px] lg:w-[557px] h-[229px] lg:h-[539px] order-2" 
          initial="hidden"
          animate={inView ? "visible" : "hidden"}
          variants={variantsImage}
         >
           <Image src='/images/mapa.png' alt="mapa" width={1000} height={1000} className='w-full h-full'/>
         </motion.div>
-      </div>
-      <div className=" flex flex-col gap-[8px]">
+      
         <motion.h2 
         ref={ref}
          initial="hidden"
          animate={inView ? "visible" : "hidden"}
          variants={variants}
-        className="font-header w-[293px] lg:w-[532px] text-[22px] lg:text-[40px] font-bold leading-[25px] lg:leading-[44px] text-center lg:text-left">
+        className="font-header w-[293px] lg:w-[532px] text-[22px] lg:text-[40px] font-bold leading-[25px] lg:leading-[44px] text-center lg:text-left order-1">
           {traduccion.servicio.header}
           <br/>
           <span className="titulo2">{traduccion.servicio.accent}</span>
@@ -72,7 +72,7 @@ function Servicio() {
         initial="hidden"
         animate={inView ? "visible" : "hidden"}
         variants={variants}
-        className="w-[289px] lg:w-[532px]  font-paragraph text-[12px] lg:text-[20px] leading-[20.52px] lg:leading-[34px] text-center lg:text-left ">{traduccion.servicio.paragraph}</motion.p>
+        className="w-[289px] lg:w-[532px]  font-paragraph text-[12px] lg:text-[20px] leading-[20.52px] lg:leading-[34px] text-center lg:text-left order-3">{traduccion.servicio.paragraph}</motion.p>
       </div>
     </div>
    
