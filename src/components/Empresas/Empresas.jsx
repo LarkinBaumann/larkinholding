8b7a78ea0023fce1b0888bae1197c11c302c8d1b
hyzmael88 <bartttt15@hotmail.com>
2024-03-14 23:13:12 -0600

    Refactor Empresas component and add utility classes for hiding scrollbars

--- src/components/Empresas/Empresas.jsx
+++ src/components/Empresas/Empresas.jsx
@@ -48,15 +48,73 @@ function Empresas() {
   return (
     <div
     id="Empresas"
-    className="h-[1024px] w-full  text-white  relative z-20">
+    className="flex h-[400px] lg:h-[1024px] w-full  text-white  relative z-20 px-4 ">
       <Image
         src="/images/empresas2.png"
         width={5000}
         height={5000}
         alt="fondo-empresas"
-        className="w-full h-full object-cover  absolute -z-50"
+        className="hidden lg:flex w-full h-full object-cover  absolute -z-50"
       />
-      <motion.div className="grid grid-cols-8 w-full h-full px-14 items-center gap-9 max-w-[1444px] mx-auto min-w-sm "
+      <div className="w-full h-full flex flex-row items-center overflow-x-scroll no-scrollbar gap-[7px]">
+        {
+          traduccion.empresas.map((empresa, index) => (
+            <div
+              key={index}
+              className="w-[184px] h-[350px] flex flex-col justify-between items-center bg-[#183F5C2E] rounded-[22px]">
+                <div className="w-[184px] h-[136px]">
+                <Image
+                    src={empresa.imagen}
+                    width={1000}
+                    height={1000}
+                    alt={empresa.nombre}
+                    className={
+                  "w-full h-full object-cover rounded-t-[22px]"
+                  }
+                  />
+                  </div>
+                  <div className="w-[101px] h-[38px] px[6px] py-[15px] flex flex-row justify-center items-center
+                  rounded-[5px]
+                  "
+                  style={{ background: 'rgba(11, 32, 46, 0.30)' }}
+                  >
+                    <p className="text-[12px] leading-[15px]
+                    text-center">
+                    {empresa.nombre}
+                    </p>
+                  </div>
+              <p className="text-center font-paragraph  leading-[15px] text-[12px] w-full px-[13px] h-[64px]">
+                {empresa.descripcion}
+              </p>
+              <div>
+              <button className="text-white w-[82px] h-[20px]  bg-[#005E93]  cursor-pointer 
+              mb-4
+              ">
+                    <div className=" w-full h-full flex justify-center items-center gap-[6px] text-[9px] font-paragraph cursor-pointer ">
+
+                   {traduccion.empresaBoton}{" "}
+                    <span>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="9"
+                        height="9"
+                        viewBox="0 0 16 16"
+                        fill="none"
+                      >
+                        <path
+                          d="M15.7392 1.60058C15.7392 1.063 15.3035 0.627207 14.7659 0.627206L6.00553 0.627207C5.46795 0.627207 5.03216 1.063 5.03216 1.60058C5.03216 2.13816 5.46795 2.57395 6.00553 2.57395L13.7925 2.57395L13.7925 10.3609C13.7925 10.8985 14.2283 11.3343 14.7659 11.3343C15.3035 11.3343 15.7392 10.8985 15.7392 10.3609L15.7392 1.60058ZM1.82695 15.9161L15.4542 2.28886L14.0776 0.912301L0.450394 14.5395L1.82695 15.9161Z"
+                          fill="white"
+                        />
+                      </svg>
+                    </span>
+                  </div>
+                  </button>
+                  </div>
+            </div>
+          ))
+        }
+      </div>
+      <motion.div className="hidden lg:grid grid-cols-8 w-full h-full px-14 items-center gap-9 max-w-[1444px] mx-auto min-w-sm "
       initial="hidden"
       animate={inView ? "visible" : "hidden"}
       variants={containerVariants}
