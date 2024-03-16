7609f046e61d52bc1fefbcd88236c26b8bf8ec92
hyzmael88 <bartttt15@hotmail.com>
2024-03-15 22:07:30 -0600

    Update meta tags for Larkin Baumann Holding website

--- src/pages/index.js
+++ src/pages/index.js
@@ -48,16 +48,15 @@ const inter = Inter({ subsets: ["latin"] });
     return (
       <>
       <Head>
-        <meta name="theme-color" content="#050409"/>
         <title>Larkin Baumann Holding.</title>
-        <meta name="description" content="" />
-        <meta name="keywords" content="" />
+        <meta name="description" content="Descubre Larkin Baumann Holding, empresa líder en diversas industrias a nivel global desde 1997. Nuestra holding se especializa en la creación y gestión de empresas en sectores que van desde la seguridad privada hasta la construcción de viviendas. Con un enfoque en la excelencia y la responsabilidad, somos la elección confiable para tus necesidades empresariales." />
+        <meta name="keywords" content="seguridad privada, seguridad, finanzas, metales preciosos, oro, metales, hierro, viviendas, mineria, real estate,  petroleo, gas, inversion, inversiones, industrial, industria, proteccion, gestion financiera, diesel, propiedades, escoltas" />
         <meta property="og:title" content="Larkin Baumann Holding" />
         <meta property="og:description" content="" />
         <meta property="og:image" content="/images/ogimage.png" />
         <meta property="og:url" content="www.lbholding.com" />
         <meta name="twitter:card" content="/images/ogimage.png" />
-      </Head>
+      </Head>
       <main className="w-full h-full">
         <div
         {...handlers}
