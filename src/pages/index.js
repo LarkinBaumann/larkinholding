3a032642cca122bc3e7f0d16ac2c36da374962ef
hyzmael88 <31554724+hyzmael88@users.noreply.github.com>
2024-03-19 03:28:49 -0600

    Update meta image URLs

--- src/pages/index.js
+++ src/pages/index.js
@@ -94,10 +94,10 @@ useEffect(() => {
   <meta name="keywords" content="seguridad privada, seguridad, finanzas, metales preciosos, oro, metales, hierro, viviendas, mineria, real estate,  petroleo, gas, inversion, inversiones, industrial, industria, proteccion, gestion financiera, diesel, propiedades, escoltas" />
   <meta property="og:title" content="Larkin Baumann Holding" />
   <meta property="og:description" content="Descubre Larkin Baumann Holding, empresa líder en diversas industrias a nivel global desde 1997. Nuestra holding se especializa en la creación y gestión de empresas en sectores que van desde la seguridad privada hasta la construcción de viviendas. Con un enfoque en la excelencia y la responsabilidad, somos la elección confiable para tus necesidades empresariales." />
-  <meta property="og:image" content="https://www.lbholding.com/assets/ogimage.jpg" />
+  <meta property="og:image" content="/assets/ogimage.jpg" />
   <meta property="og:url" content="https://www.lbholding.com" />
   <meta name="twitter:card" content="summary_large_image" />
-  <meta name="twitter:image" content="https://www.lbholding.com/assets/ogimage.jpg" />
+  <meta name="twitter:image" content="/assets/ogimage.jpg" />
 </Head>
       <main className="w-full h-full bg-[#000206]">
         <div
