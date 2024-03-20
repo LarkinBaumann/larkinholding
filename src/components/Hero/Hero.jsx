c4b1e848c8dbf4bc585c515dbbef72d4f799dba9
hyzmael88 <31554724+hyzmael88@users.noreply.github.com>
2024-03-19 18:34:43 -0600

    Update styles in Empresas and Hero components

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -69,7 +69,7 @@ const childVariants = {
       <Image src='/assets/logoHero.png' alt="hero" width={1000} height={1000} className='hidden lg:flex w-[497px]' />
      
 <motion.h2
-  className={`${heroImage == 1 ? "w-[280px] xl:w-[500px] 2xl:w-[350px] text-[68px] lg:text-8xl":
+  className={`${heroImage == 0 ? "w-[280px] lg:w-[400px] xl:w-[500px] 2xl:w-[350px] text-[68px] lg:text-8xl":heroImage == 1 ? "w-[280px] lg:w-[300px] xl:w-[500px] 2xl:w-[350px] text-[68px] lg:text-8xl":
    heroImage == 2?" w-[500px]  lg:w-[656px] text-[60px] lg:text-8xl leading-[80.55px]" : 
    heroImage == 3? "lg:w-[600px]"
    :
