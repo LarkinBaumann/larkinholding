c98271dc97478310525ece58ff08ca0efc2a8a0a
hyzmael88 <31554724+hyzmael88@users.noreply.github.com>
2024-03-19 18:13:54 -0600

    Update width of heading element in Empresas component

--- src/components/Empresas/Empresas.jsx
+++ src/components/Empresas/Empresas.jsx
@@ -239,7 +239,7 @@ function Empresas() {
             <h3
               className={
                 seleccion != index
-                  ? " w-[160px] font-header font-bold text-[28px]  uppercase leading-[35px] "
+                  ? " w-[170px] font-header font-bold text-[28px]  uppercase leading-[35px] "
                   : "hidden"
               }
             >
