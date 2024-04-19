cc925f8b09a5623c71bbf857f6283fc987c9d2a9
hyzmael88 <31554724+hyzmael88@users.noreply.github.com>
2024-04-19 01:20:02 -0600

    Update og:image paths in index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -116,10 +116,10 @@ const router = useRouter();
         <meta name="keywords" content="seguridad privada, seguridad, escoltas, blindaje, protección, emergencia, investigación, auditoria, ejecutivo" />
         <meta property="og:title" content="Larkin Baumann Security" />
         <meta property="og:description" content="Descubre Larkin Baumann Private Security, líder en protección ejecutiva y servicios de seguridad desde 1997. Ofrecemos transporte seguro, vehículos blindados, auditoría forense, respuesta a emergencias e investigaciones de antecedentes. Con un enfoque en la excelencia y la responsabilidad, somos tu elección confiable en seguridad privada." />
-        <meta property="og:image" content="/assets/ogimage.jpg" />
+        <meta property="og:image" content="/assetsSecurity/ogimage.jpg" />
         <meta property="og:url" content="https://www.lbsecurity.com" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:image" content="/assets/ogimage.jpg" />
+        <meta name="twitter:image" content="/assetsSecurity/ogimage.jpg" />
         <link rel="icon" href="favicon.ico" />
       </Head>
       }
