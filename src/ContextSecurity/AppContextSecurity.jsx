764cbc75adb2db1f168979d8e1f10f1161cdb904
hyzmael88 <31554724+hyzmael88@users.noreply.github.com>
2024-05-03 02:09:33 -0600

    Update language codes to use UAE instead of AE

--- src/ContextSecurity/AppContextSecurity.jsx
+++ src/ContextSecurity/AppContextSecurity.jsx
@@ -15,25 +15,71 @@ export const AppProviderSecurity = ({ children }) => {
     // Define el estado o cualquier otra lógica que necesites
     const [data, setData] = useState([]);
     const [idioma, setIdioma] = useState({
-        nombre: "ES",
-        code: "MX",
+       
       })
       const [traduccion, setTraduccion] = useState(ES)
 
       useEffect(() => {
+        const savedLang = localStorage.getItem("idioma");
+        if (savedLang === "ES") {
+            setIdioma({
+                nombre: 'ES',
+                code: 'MX',
+            });
+        } else if (savedLang === "EN") {
+            setIdioma({
+                nombre: 'EN',
+                code: 'US',
+            });
+        } else if (savedLang === "ZHS") {
+            setIdioma({
+                nombre: 'ZH-S',
+                code: 'CN',
+            });
+        } else if (savedLang === "ZHT") {
+            setIdioma({
+                nombre: 'ZH-T',
+                code: 'TW',
+            });
+        } else if (savedLang === "FR") {
+            setIdioma({
+                nombre: 'FR',
+                code: 'FR',
+            });
+        } else if (savedLang === "UAE") {
+            setIdioma({
+                nombre: 'UAE',
+                code: 'AE',
+            });
+        } else {
+            setIdioma({
+                nombre: 'ES',
+                code: 'MX',
+            });
+        }
+    }, []);
+ 
+      useEffect(() => {
         if (idioma.nombre === "ES") {
           setTraduccion(ES)
+          localStorage.setItem("idioma", "ES")
         } else if (idioma.nombre === "EN") {
           setTraduccion(EN)
+          localStorage.setItem("idioma", "EN")
         } else if (idioma.nombre === "ZH-S") {
           setTraduccion(ZHS)
+          localStorage.setItem("idioma", "ZHS")
         } else if (idioma.nombre === "ZH-T") {
           setTraduccion(ZHT)
+          localStorage.setItem("idioma", "ZHT")
         }
          else if (idioma.nombre === "FR") {
           setTraduccion(FR)
-        } else if (idioma.nombre === "UAE") {
+          localStorage.setItem("idioma", "FR")
+        }
+         else if (idioma.nombre === "UAE") {
           setTraduccion(AE)
+          localStorage.setItem("idioma", "AE")
         }
       }, [idioma])
 
